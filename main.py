40f2dcdf6cf802b7e12279a1a85041c0f995f073
Tucker Komasa <70603965+tkomasa@users.noreply.github.com>
2021-10-08 10:44:39 -0700

    Update main.py
    added notes and comments to be more clear

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+# imports, both are built-in modules
 import json
 import random
 
+
+# will add whatever dictionary is provided to a JSON file in an array
 def log_to_json(dict):
     with open("logs.json", mode='r', encoding='utf-8') as f:
             feeds = json.load(f)
@@ -9,7 +12,9 @@ def log_to_json(dict):
         entry = dict
         feeds.append(entry)        
         json.dump(feeds, feedsjson, indent=2)
+
         
+# function to parse the data we created by searching for the 'message_id' provided and returning to python as a dictionary
 def parse_from_json(message_id):
     f = open("logs.json", mode='r', encoding='utf-8')
     data_list = json.load(f)
@@ -21,7 +26,7 @@ def parse_from_json(message_id):
 
 
 
-
+# this just makes random entries for me to demonstrate with
 int = random.randint(0, 100000000)
 new_item = {"log_id": int,
             "data": {
@@ -35,5 +40,6 @@ new_item = {"log_id": int,
                 }
             }   
 
-#log_to_json(new_item)  # will make new log entries
+# calling the functions:
+log_to_json(new_item)  # will make new log entries
 print(parse_from_json(24502273))  # will search for a log given a log_id number (int)
