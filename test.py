b7c143672b859895bd94bccc6e881a84fb3fc566
Tucker Komasa <tskomasa@gmail.com>
2021-10-27 21:06:05 -0700

    finish

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import main as turtle_json
 
-test = turtle_json.change_log_byID(45367534562354325, "Admitted", True)
+test = turtle_json.change_log_byID(45367534562354325, "Notes", "jklsahfkdjsahfkjsdhak")
 
 print(test)
